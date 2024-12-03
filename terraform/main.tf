a956f9e239cc1cc2698c531e1a457eea4b9a9644
Shravya Panumati <151785937+ShravyaPanumati@users.noreply.github.com>
2024-12-02 21:09:32 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,21 +3,15 @@ provider "google" {
   region  = var.region
 }
 
+# Modify to manage the existing instance
 resource "google_compute_instance" "flask-app" {
-  name         = "flask-app"
-  machine_type = "f1-micro"
-  zone         = "us-central1-a"
+  name         = "flask-app"  # Name of the existing instance
+  machine_type = "f1-micro"   # You can keep the original type if it's the same
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
+  zone         = "us-central1-a"  # Zone where your instance exists
 
-  network_interface {
-    network = "default"
-    access_config {} # Enables external IP
-  }
+  # Note: Remove disk and network settings if the instance already has a configuration
+  # These should match the current settings of the existing instance, not change them.
 
   metadata_startup_script = <<EOT
 #!/bin/bash
@@ -26,13 +20,13 @@ sudo apt-get install -y python3-pip
 pip3 install flask
 EOT
 }
-
+# Modify to manage the existing Cloud SQL instance
 resource "google_sql_database_instance" "default" {
-  name             = "example-instance"
-  database_version = "MYSQL_5_7"
+  name             = "example-instance"  # Name of the existing Cloud SQL instance
+  database_version = "MYSQL_5_7"         # The version should match the existing instance's version
   region           = var.region
 
   settings {
-    tier = "db-f1-micro"
+    tier = "db-f1-micro"  # Make sure this matches the tier of the existing instance
   }
 }
