a281b4e3a0a682228881173e96ff6c36362ae3a9
Shravya Panumati <151785937+ShravyaPanumati@users.noreply.github.com>
2024-12-02 20:30:33 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,38 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
 resource "google_compute_instance" "flask-app" {
-  name         = "flask-app-instance"
+  name         = "flask-app"
   machine_type = "f1-micro"
   zone         = "us-central1-a"
-  project      = "strategic-reef-435523-j1"
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"  # Use a more recent Debian image
+      image = "debian-cloud/debian-11"
     }
   }
 
   network_interface {
     network = "default"
-    access_config {}
+    access_config {} # Enables external IP
   }
 
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    sudo apt-get update
-    sudo apt-get install -y python3-pip python3-dev
-    sudo apt-get install -y mysql-client
-    pip3 install -r /path/to/your/app/requirements.txt
-    python3 /path/to/your/app/run.py
-  EOT
+  metadata_startup_script = <<EOT
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y python3-pip
+pip3 install flask
+EOT
+}
+
+resource "google_sql_database_instance" "default" {
+  name             = "example-instance"
+  database_version = "MYSQL_5_7"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+  }
 }
