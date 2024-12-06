bb7a368d744d8074e30d95850dfe7e3292abb813
Shravya Panumati <151785937+ShravyaPanumati@users.noreply.github.com>
2024-12-06 16:31:19 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,22 @@ variable "region" {
   description = "The region for GCP resources"
   default     = "us-central1"
 }
+
+variable "db_username" {
+  description = "The username for the Cloud SQL database"
+  type        = string
+  default     = "root"
+}
+
+variable "db_password" {
+  description = "The password for the Cloud SQL database"
+  type        = string
+  default     = "password123"  # Replace with a secure password
+}
+
+variable "db_name" {
+  description = "The name of the database in the Cloud SQL instance"
+  type        = string
+  default     = "flask_db"
+}
+
