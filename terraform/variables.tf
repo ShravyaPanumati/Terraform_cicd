976e26d1d47f98a6f281b581bbec8dd42fd3961f
Shravya Panumati <151785937+ShravyaPanumati@users.noreply.github.com>
2024-12-08 14:00:42 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,9 @@ variable "region" {
   description = "The region for resources"
   type        = string
 }
+
+variable "zone" {
+  description = "The zone where resources will be created"
+  type        = string
+  default     = "us-central1-a"  # Set a default value if desired
+}
