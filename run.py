7e5d0e90f557be0cead57bf628a39fae8d6f170e
Shravya Panumati <151785937+ShravyaPanumati@users.noreply.github.com>
2024-12-08 13:49:33 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 # run.py
-from app import app  # Import the Flask app instance
 
+from app import app, db
+from app.models import Item
+
+# Create tables
+with app.app_context():
+    db.create_all()
+
+# Run Flask app
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
