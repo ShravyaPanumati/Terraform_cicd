ac1d47b299d52953dae71478bd01f4053aa71688
Shravya Panumati <151785937+ShravyaPanumati@users.noreply.github.com>
2024-12-09 11:14:14 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,13 +35,3 @@ resource "google_compute_instance" "default" {
     EOT
   }
 }
-
-output "instance_name" {
-  description = "The name of the Compute Engine instance"
-  value       = google_compute_instance.default.name
-}
-
-output "instance_external_ip" {
-  description = "The external IP address of the Compute Engine instance"
-  value       = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
-}
