d33774511e69d258134b98bbb7224e29f86fff4d
Shravya Panumati <151785937+ShravyaPanumati@users.noreply.github.com>
2024-12-08 19:28:19 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,53 +1,31 @@
+# Provider configuration
 provider "google" {
   project = var.project_id
   region  = var.region
 }
 
-resource "google_sql_database_instance" "default" {
-  name             = "example-instance"
-  database_version = "MYSQL_8_0"
-  settings {
-    tier = "db-f1-micro"
+# Create a Google Cloud Storage bucket to host the application
+resource "google_storage_bucket" "static_site" {
+  name     = "${var.project_id}-static-site"
+  location = var.region
+  force_destroy = true
+
+  website {
+    main_page_suffix = "index.html"
   }
 }
 
-resource "google_sql_database" "default" {
-  name     = "flask_db"
-  instance = google_sql_database_instance.default.name
+# Grant public access to the bucket contents
+resource "google_storage_bucket_iam_binding" "public" {
+  bucket = google_storage_bucket.static_site.name
+
+  role = "roles/storage.objectViewer"
+  members = [
+    "allUsers",
+  ]
 }
 
-resource "google_sql_user" "default" {
-  name     = "root"
-  instance = google_sql_database_instance.default.name
-  password = "password123"
-}
-
-resource "google_compute_network" "default" {
-  name = "flask-network"
-}
-
-resource "google_compute_instance" "default" {
-  name         = "flask-app"
-  machine_type = "e2-micro"
-  zone         = var.zone  # Add zone here
-
-  boot_disk {
-    initialize_params {
-      image = "projects/debian-cloud/global/images/debian-11-bullseye-v20230509"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.default.name
-  }
-
-  metadata = {
-    startup-script = <<EOT
-      #! /bin/bash
-      sudo apt-get update
-      sudo apt-get install -y python3 python3-pip
-      pip3 install -r /path/to/requirements.txt
-      python3 run.py
-    EOT
-  }
+# Output the bucket URL
+output "bucket_url" {
+  value = "https://storage.googleapis.com/${google_storage_bucket.static_site.name}"
 }
