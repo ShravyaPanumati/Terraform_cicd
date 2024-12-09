2f94229db32c9fc63e08bb14686449a3b71b0d88
Shravya Panumati <151785937+ShravyaPanumati@users.noreply.github.com>
2024-12-08 19:55:12 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,9 @@
-output "bucket_name" {
-  description = "The name of the created bucket"
-  value       = google_storage_bucket.static_site.name
+output "instance_name" {
+  description = "The name of the Compute Engine instance"
+  value       = google_compute_instance.default.name
+}
+
+output "instance_external_ip" {
+  description = "The external IP address of the Compute Engine instance"
+  value       = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
 }
